3aa836755d46640e749efcc41cb48dd0dccbdccb
Ahad Hossain <ahadxx99@gmail.com>
2026-07-09 00:33:14 +0600

    category module added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import authRoutes from "../modules/auth/auth.route";
 import userRoutes from "../modules/user/user.route";
+import categoryRoutes from "../modules/category/category.route";
 
 const router = Router();
 
@@ -17,5 +18,6 @@ router.get("/", (_req, res) => {
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
+router.use("/categories", categoryRoutes);
 
 export default router;
