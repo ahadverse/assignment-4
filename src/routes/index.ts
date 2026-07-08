073d4270e925154d08971f554866b212182f299d
Ahad Hossain <ahadxx99@gmail.com>
2026-07-09 00:16:14 +0600

    user module added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import authRoutes from "../modules/auth/auth.route";
+import userRoutes from "../modules/user/user.route";
 
 const router = Router();
 
@@ -15,5 +16,6 @@ router.get("/", (_req, res) => {
 });
 
 router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
 
 export default router;
